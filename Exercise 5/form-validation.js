3bb1f7f9c2d2941fa17875f335aff870d9e918b9
alvio <alviona.manco17@hotmail.com>
2023-11-24 19:33:30 +0200

    21th Session - minor changes in Exercise 5

--- Exercise 5/form-validation.js
+++ Exercise 5/form-validation.js
@@ -1,8 +1,11 @@
 window.addEventListener('load', function () {
+
     let form = document.getElementById('sign_up')
+
+    /* Age validation; The website is intended for users aged 13 and above */
     let enteredBday = form.birthday
 
-    enteredBday.onchange = function () {
+    enteredBday.addEventListener('change', function () {
         const enteredBdayDate = new Date(enteredBday.value)
         const currentDate = new Date()
 
@@ -11,34 +14,34 @@ window.addEventListener('load', function () {
         } else {
             enteredBday.setCustomValidity('')
         }
-    }
+    })
 
+    /* Password matching; Passwords should be identical */
     let password = form.password
     let confirmPassword = form.confirm_password
 
-
-    let submitBtn = form.querySelector('button[type="submit"]')
-
-    submitBtn.addEventListener('click', function () {
-        // if (password.value === null){
-        //     confirmPassword.setCustomValidity('You should type in your password first and then confirm it')
+    confirmPassword.addEventListener('change', function () {
         if (password.value !== confirmPassword.value) {
             confirmPassword.setCustomValidity('Passwords don\'t match')
         } else {
             confirmPassword.setCustomValidity('')
         }
+    })
+
+
+    /* At least one contact option selected */
+    let submitBtn = form.querySelector('button[type="submit"]') /* This is checked on submit */
+
+    submitBtn.addEventListener('click', function () {
 
         let uncheckedCheckboxes = form.querySelectorAll('input[name="contact_option"]:not(:checked)')
         let checkboxes = form.querySelectorAll('input[name="contact_option"]')
-        
+
         if (uncheckedCheckboxes.length === checkboxes.length) {
             uncheckedCheckboxes[0].setCustomValidity('You should select at least 1 option')
         } else {
             checkboxes[0].setCustomValidity('')
         }
     })
-
-
-
 
 })
