ebb4ad1ff2819772e0b26ef7bad13862cff7ec68
Panos <triantpanos30@gmail.com>
2023-12-23 21:45:15 +0200

    1s session 2nd task - minor changes

--- public/js/category.js
+++ public/js/category.js
@@ -13,11 +13,22 @@ function fillTemplate() {
     const urlParams = new URLSearchParams(window.location.search);
     const categoryId = urlParams.get('id');
     let url = `https://wiki-ads.onrender.com/ads?category=${categoryId}`;
-    console.log(url)
+
     fetch(url, init)
     .then(response => response.json())
     .then(obj => {
         console.log(obj);
+        // Load the template
+        let ads = document.getElementById('category-template').textContent;
+        window.templates = {};
+        window.templates.ads = Handlebars.compile(ads);
+
+        // Fill the template
+        let contentAds = document.getElementById("example");
+        let htmlContent = templates.ads({
+            array: obj
+        });
+        contentAds.innerHTML = htmlContent;
     })
     .catch(error => {
         console.error('Error fetching data:', error);
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // Fetch data
 
-generalUrl = "https://wiki-ads.onrender.com/"
+// generalUrl = "https://wiki-ads.onrender.com/"
 
 let myHeaders = new Headers();
 myHeaders.append('Accept', 'application/json');
@@ -18,12 +18,12 @@ function fillTemplate() {
     url = "https://wiki-ads.onrender.com/categories"
     fetch(url, init)
     .then(response => response.json())
-    .then(obj => {
-        // Create an array of promises for fetching subcategories static/images/home-category.PNG
-        let subCategoryPromises = obj.map(item => fetchSubCategory(item.id));
+    .then(categoriesList => {
+        // Create an array of promises for fetching subcategories
+        let subCategoryPromises = categoriesList.map(item => fetchSubCategory(item.id));
 
         /*
-        The fetchSubCategory function performs an asynchronous operation (fetching data), . Since fetch returns a Promise, the value of item.subCategory will be undefined at the time you access it.
+        The fetchSubCategory function performs an asynchronous operation (fetching data). Since fetch returns a Promise, the value of item.subCategory will be undefined at the time you access it.
 
         To handle asynchronous operations and ensure that the subcategory data is assigned to the correct item, you can use Promise.all to wait for all fetch requests to complete before updating the items
          */
@@ -31,25 +31,24 @@ function fillTemplate() {
         return Promise.all(subCategoryPromises)
             .then(subCategories => {
                 // Assign subcategories to corresponding items
-                obj.forEach((item, index) => {
+                categoriesList.forEach((item, index) => {
                     item.subCategory = subCategories[index];
                 });
-                return obj;
+                return categoriesList;
             });
     })
-    .then(obj => {
+    .then(categoriesList => {
         // Load the template
         let categories = document.getElementById('index-template').textContent;
         window.templates = {};
         window.templates.categories = Handlebars.compile(categories);
 
         // Fill the template
-        let pC = document.getElementById("example");
+        let contentTemplate = document.getElementById("example");
         let htmlContent = templates.categories({
-            array: obj
+            array: categoriesList
         });
-        pC.innerHTML = htmlContent;
-        console.log(obj);
+        contentTemplate.innerHTML = htmlContent;
     })
     .catch(error => {
         console.log(error);
@@ -61,9 +60,6 @@ function fetchSubCategory(id) {
 
     return fetch(url, init)
         .then(response => response.json())
-        .then(obj => {
-            return obj;
-        })
         .catch(error => {
             console.log(error);
         });
