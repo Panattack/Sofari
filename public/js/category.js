1bcf71f896cb76a146dd512465c2d33ef53db433
alvio <alviona.manco17@hotmail.com>
2023-12-24 22:19:15 +0200

    2nd Session, 2nd task - category template and js

--- public/js/category.js
+++ public/js/category.js
@@ -16,25 +16,70 @@ function fillTemplate() {
     const categoryId = urlParams.get('id');
     let url = `${generalUrl}ads?category=${categoryId}`;
 
+    window.templates = {};
+
     // Fetch the advertisements of the specified category
     fetch(url, init)
-    .then(response => response.json())
-    .then(adsList => {
-        console.log(adsList);
+        .then(response => response.json())
+        .then(adsList => {
 
-        // Load the template
-        let ads = document.getElementById('category-template').textContent;
-        window.templates = {};
-        window.templates.ads = Handlebars.compile(ads);
+            // Load the template for the categories
+            let adsTemplate = document.getElementById('category-template').textContent;
+            window.templates.adsTemplate = Handlebars.compile(adsTemplate);
 
-        // Fill the template
-        let adsCategoryProducts = document.getElementById("ads-category-products");
-        let htmlContent = templates.ads({
-            array: adsList
+            // Fill the template
+            let adsCategoryProducts = document.getElementById("ads-category-products");
+            let htmlContent = templates.adsTemplate({
+                array: adsList
+            });
+            adsCategoryProducts.innerHTML = htmlContent;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
         });
-        adsCategoryProducts.innerHTML = htmlContent;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+
+    // Load the templates for tips and trends
+    let tipsTemplate = document.getElementById('tips-template').textContent;
+    let trendsTemplate = document.getElementById('trends-template').textContent;
+
+    window.templates.tipsTemplate = Handlebars.compile(tipsTemplate);
+    window.templates.trendsTemplate = Handlebars.compile(trendsTemplate);
+
+    // Fill the templates
+    let tips = document.getElementById("tips");
+    let trends = document.getElementById("trends");
+
+    // tips_trendsJSON is a global variable loaded from data/js/tips_trends.js script
+    let tips_trends = JSON.parse(tips_trendsJSON).find(item => item["category_id"] == categoryId);
+
+    if (!tips_trends) {
+        tips_trends = { "trends": [], "tips": [] };
+    }
+
+    let trendsContent = window.templates.trendsTemplate({
+        array: tips_trends["trends"],
+        empty: !tips_trends || !tips_trends["trends"] || tips_trends["trends"].length === 0
+
     });
+    trends.innerHTML = trendsContent;
+
+    let tipsContent = window.templates.tipsTemplate({
+        array: tips_trends["tips"],
+        empty: !tips_trends || !tips_trends["tips"] || tips_trends["tips"].length === 0
+    });
+    tips.innerHTML = tipsContent;
+
+    // Load the template for the category name
+    let categoryLinkTemplate = document.getElementById("category-link-template").textContent;
+    window.templates.categoryLinkTemplate = Handlebars.compile(categoryLinkTemplate)
+
+    // Get the category name
+    const categoryName = urlParams.get('category');
+
+    // Fill the template
+    let category = document.getElementById("category")
+    let categoryLinkContent = window.templates.categoryLinkTemplate({
+        category: categoryName
+    });
+    category.innerHTML = categoryLinkContent;
 }
---
+++ public/js/tips_trends.js
@@ -0,0 +1,50 @@
+var tips_trendsJSON = `[
+    
+    {
+        "category_id": 1,
+        "tips": [
+              "Consider Lifestyle and Future Needs When Choosing a Home",
+              "Ensure Essential Safety Features in the Property",
+              "Regularly Maintain Your House for Preventive Care",
+              "Explore Interior Design for Comfortable Living",
+              "Research Real Estate Market Trends and Values",
+              "Opt for Energy-Efficient Home Systems",
+              "Investigate Neighborhood Amenities and Community"
+        ],
+
+        "trends": [
+              "Embrace Smart Home Integration for Convenience",
+              "Choose Eco-Friendly Features for Sustainable Living",
+              "Look for Homes with Dedicated Office Spaces",
+              "Opt for Open Floor Plans for Spacious Living",
+              "Consider Properties with Outdoor Living Spaces",
+              "Opt for Multigenerational Living-Friendly Homes",
+              "Equip Your Home with Smart Appliances for Convenience"
+        ]
+    },
+
+
+    {
+        "category_id": 3,
+        "tips": [
+
+            "Consider your riding style when choosing a bike",
+            "Check for safety features like ABS and traction control",
+            "Proper maintenance extends the life of your Motorcycle",
+            "Explore different types of riding gear for safety",
+            "Research the resale value of the Motorcycle brand",
+            "Test ride before making a final decision"
+        ],
+
+        "trends": [
+
+            "Electric Motorcycles on the Rise",
+            "Adventure Touring Bikes in Demand",
+            "Customization Culture in Motorcycling",
+            "Integration of Smart Technology in Bikes",
+            "Rise of Retro and Classic Bike Designs",
+            "Lightweight and Nimble Commuter Bikes"
+        ]
+    }
+
+]`
