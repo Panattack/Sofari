91b44e3a87f8db37190b25b7f767cba8d06cda5f
alvio <alviona.manco17@hotmail.com>
2023-12-25 00:41:45 +0200

    2nd Session, 2nd Task - Bonus1 (filters) implemented

---
+++ ToDo.md
@@ -0,0 +1,7 @@
+Questions:
+- 2 Fetches that are not concurrent in main.js
+- Errors control
+
+To Do:
+- CSS for filters (subcategories)
+- CSS for subcategory.html
--- public/js/category.js
+++ public/js/category.js
@@ -8,29 +8,56 @@ let init = {
     headers: myHeaders
 }
 
+let advertisements = undefined
+
 window.addEventListener('load', fillTemplate);
 
 function fillTemplate() {
     // Get the category id from the url of the page
     const urlParams = new URLSearchParams(window.location.search);
     const categoryId = urlParams.get('id');
-    let url = `${generalUrl}ads?category=${categoryId}`;
 
     window.templates = {};
 
+
+    let urlSubcategories = `${generalUrl}categories/${categoryId}/subcategories`
+
+    // Fetch the subcategories of the specified category
+    fetch(urlSubcategories, init)
+        .then(response => response.json())
+        .then(subcategoriesList => {
+
+            // Load the template for the filters
+            let subcategoryFilterTemplate = document.getElementById("subcategory-filter-template").textContent;
+            window.templates.subcategoryFilterTemplate = Handlebars.compile(subcategoryFilterTemplate);
+
+            //Fill the template
+            let subcategoryPair = document.getElementById("subcategory-input-label-pair")
+            let subcategoryPairContent = templates.subcategoryFilterTemplate({
+                array: subcategoriesList
+            });
+            subcategoryPair.innerHTML = subcategoryPairContent;
+
+        });
+
+
+    let urlCategory = `${generalUrl}ads?category=${categoryId}`;
+
     // Fetch the advertisements of the specified category
-    fetch(url, init)
+    fetch(urlCategory, init)
         .then(response => response.json())
         .then(adsList => {
+            advertisements = adsList
 
             // Load the template for the categories
-            let adsTemplate = document.getElementById('category-template').textContent;
+            let adsTemplate = document.getElementById("category-template").textContent;
             window.templates.adsTemplate = Handlebars.compile(adsTemplate);
 
             // Fill the template
             let adsCategoryProducts = document.getElementById("ads-category-products");
+
             let htmlContent = templates.adsTemplate({
-                array: adsList
+                array: advertisements
             });
             adsCategoryProducts.innerHTML = htmlContent;
         })
@@ -83,3 +110,38 @@ function fillTemplate() {
     });
     category.innerHTML = categoryLinkContent;
 }
+
+
+
+document.addEventListener("DOMContentLoaded", filterAds)
+
+function filterAds() {
+    var div = document.getElementById('subcategory-input-label-pair');
+
+    div.addEventListener('click', function (event) {
+        var clickedElement = event.target;
+
+        if (clickedElement.type === "radio" && clickedElement.name === "subcategory") {
+            var clickedId = clickedElement.id;
+            console.log('Clicked Radio Button ID:', clickedId);
+
+            if (advertisements !== undefined) {
+
+                // Load the template for the categories
+                let adsTemplate = document.getElementById("category-template").textContent;
+                window.templates.adsTemplate = Handlebars.compile(adsTemplate);
+
+                // Fill the template
+                let adsCategoryProducts = document.getElementById("ads-category-products");
+
+                // Use a regex to extract the number of the subcategory, given that it is in the form x...xd...d where x: letters, d: digits
+                clickedId = clickedId.match(/\d+/g)[0]
+
+                let htmlContent = templates.adsTemplate({
+                    array: clickedId != 0 ? advertisements.filter(item => item["subcategory_id"] == clickedId) : advertisements
+                });
+                adsCategoryProducts.innerHTML = htmlContent;
+            }
+        }
+    });
+};
